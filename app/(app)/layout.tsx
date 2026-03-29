d5b579c96cd488b72a69d4d123d6a0ac2e6b3208
varfmx21 <luisvalderrabano9@gmail.com>
2026-03-28 22:19:16 -0600

    fix: fixing the cut in laptops

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,5 @@
 import { AppSidebar } from "@/components/shared/AppSideBar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen">
-      <AppSidebar>
-        <main className="flex-1 pt-16 md:pt-0">
-          {children}
-        </main>
-      </AppSidebar>
-    </div>
-  )
+  return <AppSidebar>{children}</AppSidebar>;
 }
