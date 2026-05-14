ed31aceebdc0a5b28410d495dc2e10015d21fc5b
varfmx21 <luisvalderrabano9@gmail.com>
2026-05-13 18:34:39 -0600

    contribuitors

--- data/contributors.ts
+++ data/contributors.ts
@@ -4,12 +4,12 @@ export const contributors: Contributor[] = [
   {
     id: 1,
     name: "varfmx21",
-    role: "Frontend leader, DevSecOps Engineer",
-    contribution: "Lead frontend development and UI/UX design. Making sure the app is running in services in Vercel with CI/CD pipelines and backend in VPS in Vultr with containerization using Docker and Kubernetes.",
+    role: "Frontend leader",
+    contribution: "",
     avatar: "https://github.com/varfmx21.png",
     profileUrl: "https://github.com/varfmx21",
     socials: [
-      { platform: "linkedin",  url: "www.linkedin.com/in/luisvalde" },
+      { platform: "linkedin",  url: "https://www.linkedin.com/in/luisvalde" },
       { platform: "github",   url: "https://github.com/varfmx21" },
       { platform: "web-page", url: "https://v4lde.tech/" },
     ],
@@ -17,8 +17,8 @@ export const contributors: Contributor[] = [
   {
     id: 2,
     name: "Shadow",
-    role: "Backend leader, Backend Eng",
-    contribution: "Lead backend development and database management. Ensuring the security and scalability of the application, as well as optimizing performance and implementing new features.",
+    role: "Infraestructure leader",
+    contribution: "",
     avatar: "https://github.com/JRV-XVI.png",
     profileUrl: "https://github.com/JRV-XVI",
     socials: [
@@ -29,8 +29,8 @@ export const contributors: Contributor[] = [
   {
     id: 3,
     name: "Sammy",
-    role: "Labs leader, Cybersecurity Eng",
-    contribution: "Lead the labs development and cybersecurity content creation. Designing and implementing hands-on labs that cover a wide range of cybersecurity topics, from basic concepts to advanced techniques.",
+    role: "Labs leader",
+    contribution: "",
     avatar: "https://github.com/sammy-ulfh.png",
     profileUrl: "https://github.com/sammy-ulfh",
     socials: [
@@ -42,7 +42,7 @@ export const contributors: Contributor[] = [
     id: 4,
     name: "MarioFengW",
     role: "Frontend Eng",
-    contribution: "Help to create multiple pages of the app, implementing the UI/UX design and ensuring a responsive and user-friendly experience.",
+    contribution: "",
     avatar: "https://github.com/MarioFengW.png",
     profileUrl: "https://github.com/MarioFengW",
     socials: [
@@ -54,7 +54,7 @@ export const contributors: Contributor[] = [
     id: 5,
     name: "emihdz",
     role: "UI/UX Designer",
-    contribution: "Help to design the UI/UX of the app and the website, creating wireframes, mockups and prototypes to ensure a user-friendly experience.",
+    contribution: "",
     avatar: "https://github.com/emihdz1209.png",
     profileUrl: "https://github.com/emihdz1209",
     socials: [
@@ -66,7 +66,7 @@ export const contributors: Contributor[] = [
     id: 6,
     name: "angel",
     role: "UI/UX Designer",
-    contribution: "Help to design the UI/UX of the app and the website, creating wireframes, mockups and prototypes to ensure a user-friendly experience.",
+    contribution: "",
     avatar: "https://github.com/AngelGabrielCP5TEC.png",
     profileUrl: "https://github.com/AngelGabrielCP5TEC",
     socials: [
@@ -77,14 +77,72 @@ export const contributors: Contributor[] = [
   {
     id: 7,
     name: "uno21858",
-    role: "Software Engineer, VPS assistant",
-    contribution: "Help to assist with configuration in VPS in Vultr and documentation of the API CTFd for the app. Create the OSINT lab for Hack2Dawn 2026",
+    role: "Infraestructure Eng",
+    contribution: "",
     avatar: "https://github.com/uno21858.png",
     profileUrl: "https://github.com/uno21858",
     socials: [
       { platform: "linkedin", url: " https://www.linkedin.com/in/erick-alberto-sanchez/" },
       { platform: "github",   url: "https://github.com/uno21858" },
-      { platform: "web-page", url: "uno21things.dev" }
+      { platform: "web-page", url: "https://uno21things.dev" }
     ],
+  },
+  {
+    id: 8,
+    name: "DiegoLizarraga",
+    role: "Frontend Eng",
+    contribution: "",
+    avatar: "https://github.com/DiegoLizarraga.png",
+    profileUrl: "https://github.com/DiegoLizarraga",
+    socials: [
+        { platform: "linkedin", url: "https://www.linkedin.com/in/diegolizarragasanchez/" },
+        { platform: "github",   url: "https://github.com/DiegoLizarraga" }
+    ],
+  },
+  {
+    id : 9,
+    name: "BlitzExp",
+    role: "Labs Eng",
+    contribution: "",
+    avatar: "https://github.com/BlitzExp.png",
+    profileUrl: "https://github.com/BlitzExp",
+    socials: [
+        { platform: "linkedin", url: "https://www.linkedin.com/in/jose-eduardo-najera-de-anda" },
+        { platform: "github",   url: "https://github.com/BlitzExp" }
+    ],
+  },
+  {
+    id: 10,
+    name: "ssantiago065",
+    role: "Labs Eng",
+    contribution: "",
+    avatar: "https://github.com/ssantiago065.png",
+    profileUrl: "https://github.com/ssantiago065",
+    socials: [
+        { platform: "linkedin", url: "https://www.linkedin.com/in/ssantiago065" },
+        { platform: "github",   url: "https://github.com/ssantiago065" }
+    ],
+  },
+  {
+    id: 11,
+    name: "Sammas",
+    role: "Photographer",
+    contribution: "",
+    avatar: "https://cdn.myportfolio.com/67100ecb-8d7f-4386-a01d-a4c5f5ff5e1c/62ca0411-53e3-4550-9377-600193c209d5_rwc_0x0x3456x5184x4096.jpg?h=237168d0ef86fdad46f7edc4620ceec0",
+    profileUrl: "https://sammas.myportfolio.com/eventos",
+    socials: [
+        { platform: "web-page", url: "https://sammas.myportfolio.com/eventos" },
+    ],
+  },
+  {
+    id: 12,
+    name: "AlexFC",
+    role: "Lab Eng",
+    contribution: "",
+    avatar: "https://github.com/Alexreborn129.png",
+    profileUrl: "https://github.com/Alexreborn129",
+    socials: [
+      {platform: "github", url: "https://github.com/Alexreborn129"}
+    ]
   }
 ];
